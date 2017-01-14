6e03bb50f089d85cadfca74579e8864a9a910482
Steven Graf <stevengraf@Stevens-MBP.lan>
2017-01-14 00:44:48 -0500

    completed sign in UI

---
+++ Hatchling/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Hatchling
+//
+//  Created by Steven Graf on 1/14/17.
+//  Copyright © 2017 IdeaShare. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
+
