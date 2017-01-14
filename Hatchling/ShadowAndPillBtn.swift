9be984780f4fe2375b1b8c837bc52fa223895f6b
Steven Graf <stevengraf@Stevens-MBP.lan>
2017-01-14 00:23:53 -0500

    completed linking buttons and views

---
+++ Hatchling/ShadowAndPillBtn.swift
@@ -0,0 +1,21 @@
+//
+//  ShadowAndPillBtn.swift
+//  Hatchling
+//
+//  Created by Steven Graf on 1/14/17.
+//  Copyright © 2017 IdeaShare. All rights reserved.
+//
+
+import UIKit
+
+class ShadowAndPillBtn: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
