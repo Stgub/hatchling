192d038b59613fe314a1c550729afedfe99d97d9
charlesfayal <charles.fayal@uconn.edu>
2017-01-19 21:19:02 -0500

    fixPickPicture

--- Hatchling/productPics.swift
+++ Hatchling/productPics.swift
@@ -8,17 +8,64 @@
 
 import UIKit
 
-class productPics: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class productPics: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
+    
+    var imagePicked:UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        productImagePicker.delegate = self
+
+        
+        
+        let pictureTap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.pickProductPictureTapped(_:)))
+        productPicture.isUserInteractionEnabled = true
+        productPicture.addGestureRecognizer(pictureTap)
+
+        let logoTap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.pickLogoPictureTapped(_:)))
+        logoPicture.isUserInteractionEnabled = true
+        logoPicture.addGestureRecognizer(logoTap)
+    }
     
     @IBAction func backBtnTapped(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
     
+    @IBOutlet weak var productPicture: UIImageView!
+    @IBOutlet weak var logoPicture: UIImageView!
 
+    let productImagePicker = UIImagePickerController()
 
+    @IBAction func pickLogoPictureTapped(_ sender: Any) {
+        print("picking Logo Picture")
+        imagePicked = logoPicture
+        productImagePicker.allowsEditing = true
+        
+        present(productImagePicker, animated: true, completion: nil)
+        
+    }
+
+    @IBAction func pickProductPictureTapped(_ sender: Any) {
+        print("picking Product Picture")
+         imagePicked = productPicture
+        productImagePicker.allowsEditing = true
+        
+        present(productImagePicker, animated: true, completion: nil)
+    }
 
     
-
+    //MARK: - Delegates
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : AnyObject])
+    {
+        var  chosenImage = UIImage()
+        chosenImage = info[UIImagePickerControllerOriginalImage] as! UIImage //2
+        imagePicked.contentMode = .scaleAspectFit //3
+       imagePicked.image = chosenImage //4
+        dismiss(animated:true, completion: nil) //5
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
     /*
     // MARK: - Navigation
 
