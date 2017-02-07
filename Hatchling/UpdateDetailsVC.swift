7c850f1debbf93a2bebc611cc5ecef5e3a38d68e
charlesfayal <charles.fayal@uconn.edu>
2017-02-06 20:11:27 -0500

    Made explanation and title change

--- Hatchling/UpdateDetailsVC.swift
+++ Hatchling/UpdateDetailsVC.swift
@@ -11,6 +11,10 @@ import UIKit
 class UpdateDetailsVC: UIViewController {
     var update:Update!
     
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var explanationLabel: UILabel!
+    
     @IBOutlet weak var updateDescript: textViewRoundCorners!
     @IBOutlet weak var updateLink: UITextField!
     
@@ -31,7 +35,22 @@ class UpdateDetailsVC: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        switch(update.updateType){
+        case updateTypes.launch:
+            titleLabel.text = "Launch"
+            explanationLabel.text = launchExplanation
+        case updateTypes.betaTesting:
+            titleLabel.text = "Beta Testing"
+            explanationLabel.text = betaExplanation
+        case updateTypes.preorders:
+            titleLabel.text = "Taking Preorders"
+            explanationLabel.text = preordersExplanation
+        case updateTypes.crowdfunding:
+            titleLabel.text = "Crowdfunding"
+            explanationLabel.text = crowdfundingExplanation
+        default:
+            print("Chuck: had a update type onot in update types struct or update did not get a type")
+        }
         // Do any additional setup after loading the view.
     }
 
@@ -60,5 +79,10 @@ class UpdateDetailsVC: UIViewController {
         alertController.addAction(defaultAction)
         present(alertController, animated: true, completion: nil)
     }
+    
+    private let betaExplanation = "Create a beta testing update if you are looking for people to try out your product and give you feedback. Submit a link to where beta testers can download or look at your product."
+    private let crowdfundingExplanation = "Create a crowdfunding update if you are starting a crowdfunding campaign and want followers to know. Submit a link to the campaign so followers can find it."
+    private let preordersExplanation = "Create a pre-order update if you are taking pre-orders and want to let your followers know. Submit a link to the pre-order page so followers can find it."
+    private let launchExplanation = "Create a launch update if you are officially launching your product and want all your followers know. Submit a link to the product web page so followers can find it."
 
 }
